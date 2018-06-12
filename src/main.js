332321260f64b242ae0e5903dc84d07b6b45d72b
AlejandraAAO <aventurero_7_20@hotmail.com>
2018-06-12 14:49:32 -0500

    actualizacion de html y main.js y carpeta de ejemplo

---
+++ src/main.js
@@ -0,0 +1,51 @@
+const form = document.getElementById("search-form");
+const searchField = document.getElementById("search-keyword");
+const resultBox = document.getElementById("container");
+let searchedForText;
+
+const listStudents = document.querySelector("ul");
+
+fetch("http://127.0.0.1:5500/data/cohorts/lim-2018-03-pre-core-pw/users.json")
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        for(let i=0; i<data.length; i++){
+            let students = document.createElement("li");
+            students.innerHTML = data[i].name + " ";
+            listStudents.appendChild(students);
+        }
+        //document.write("1. "+ data[0].name + " ");
+        //document.write("2. "+data[1].name);
+        })
+    ;
+
+    /*  lista completa de los readyStatevalores está documentada en XMLHTTPRequest.readyState y es la siguiente:
+0 (sin inicializar) o ( solicitud no inicializada )
+1 (carga) o ( conexión al servidor establecida )
+2 (cargado) o ( solicitud recibida )
+3 (interactivo) o ( solicitud de procesamiento )
+4 (completo) o ( solicitud finalizada y la respuesta está lista )
+*/ 
+fetch ("http://127.0.0.1:5500/data/cohorts/lim-2018-03-pre-core-pw/progress.json" )
+        .then(function(progress){
+        return progress.json();
+        })
+        .then(function(progress){
+            console.log(progress);
+        });
+
+fetch ("http://127.0.0.1:5500/data/cohorts.json")
+        .then(function(cohorts){
+        return cohorts.json();
+        })
+        .then(function(cohorts){
+        console.log(cohorts);
+        });
+
+/*form.addEventListener("submit", function(e){
+    e.preventDefault();
+    resultBox.innerHTML="";
+    searchedForText = searchField.value;
+    getNews();
+});*/
