56eab556c5b424a3de5772694811a49e1fc12b85
unknown <vmirella_17@hotmail.com>
2018-06-14 12:47:25 -0500

    procesando data

--- src/data.js
+++ src/data.js
@@ -1,15 +1,1 @@
-/* const XHR = new XMLHttpRequest();
-Console.log(XHR);
 
-XHR.onreadystatechange = function(){
-    if(XHR.readyState == 4 && XHR.status ==200){
-        console.log(JSON.parse(XHR.responseText));
-        console.log(XHR.res);
-
-    }
-    else{
-        console.log('Hay un problema en ,el servildor');
-    }
-}
-XHR.open('GET','http://127.0.0.1:5500/data/cohorts.json');
-XHR.send(); */
--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
-fetch('http://127.0.0.1:5500/data/cohorts.json')
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
-    console.log(data);
-    console.log(data[0].id)
-  });
+// fetch('http://127.0.0.1:5500/data/cohorts.json')
+//   .then(function(response) {
+//     return response.json();
+//   })
+//   .then(function(data) {
+//     console.log(data);
+//     console.log(data[0].id)
+//   });
+const XHR = new XMLHttpRequest();
+
+XHR.onreadystatechange = function(){
+    let data;
+    if(XHR.readyState == 4 && XHR.status ==200){
+        data = JSON.parse(XHR.responseText);
+        /* for(let i = 0; i < data.length; i++){
+          document.write(data[i].start);
+          document.write('<br>');
+        } */        
+    }
+    else{
+        console.log('Hay un problema en el servidor');
+    }
+    return data;
+}
+XHR.open('GET','http://127.0.0.1:5500/data/cohorts.json');
+XHR.send(); 
