0b54f597619baae136c4d70d15ddb03da517ddf1
jslyne <jslyn.vip@gmail.com>
2018-06-18 16:54:00 -0500

    cambios de main.js - agregando select

--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
-const cohortStudent = document.getElementById('student');
-cohortStudent.addEventListener('click', () => {
-    var 
+const sedeCohort = document.getElementById('sede');
+fetch('../data/cohorts.json')
+  .then(response => response.json())
+  .then(json => {
+    const sedes = json;
+    // console.log(sedes.length);
+    for (let i = 0; i < sedes.length; i++) {
+      const optionElements = document.createElement('option');
+      const contenidoOption = document.createTextNode(sedes[i].id);
+      optionElements.appendChild(contenidoOption);
+      console.log(sedes[i].id);
+      sedeCohort.appendChild(optionElements);
+    }
+  })
+  .catch((err) => {
+    console.error(err);
   });
+
+  
