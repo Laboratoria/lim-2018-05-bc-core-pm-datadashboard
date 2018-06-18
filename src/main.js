29866498fd89663a7f7b29111adce2c315e57ae2
unknown <vmirella_17@hotmail.com>
2018-06-18 11:56:35 -0500

    filtro de ejemplo

--- src/main.js
+++ src/main.js
@@ -16,23 +16,13 @@
     }
     infoAlumnas.innerHTML = output;
 });*/
+const infoAlumnas = document.getElementById('listStudents');
+const cargar = document.getElementById('cargar');
+const selectUsers = document.getElementById('users');
 
-const fileUsers = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
-const infoAlumnas = document.getElementById('infoAlumnas');
-
-fetch(fileUsers).then(function(response) {
-    return response.json();
-})
-.then(json => {
-    let alumnas = json;
-    for(let i = 0; i < alumnas.length; i++){
-        if(alumnas[i].role == 'student'){
-            const element = document.createElement('p'); //crea un <p>
-            const contenidoElement = document.createTextNode(alumnas[i].name); //
-
-            element.appendChild(contenidoElement);
-            infoAlumnas.appendChild(element);
-        }  
-    }
+cargar.addEventListener('click', () => {
+    loadStudents();
 });
+
+
 
