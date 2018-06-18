35e1a72aacf886524648e336c8caf4e50bd9447f
AlejandraAAO <aventurero_7_20@hotmail.com>
2018-06-18 18:23:51 -0500

    funcion del boton ver muestra alumnas

--- src/main.js
+++ src/main.js
@@ -1,9 +1,37 @@
+//variables de los datos que vienen del json
+const usersJson = "../data/cohorts/lim-2018-03-pre-core-pw/users.json";
+const cohortsJson = "../data/cohorts.json";
+const progressJson = "../data/cohorts/lim-2018-03-pre-core-pw/progress.json";
+//Muestra las estudiantes solo nombres
 const listStudents = document.querySelector("ul");
-var botonDeInicio = document.querySelector('button');
-const menu = document.getElementById("listCo")
+var botonDeLista = document.querySelector('button');
+const menu = document.getElementById("listCo");
 
-botonDeInicio.onclick = function () {
-fetch("../data/cohorts/lim-2018-03-pre-core-pw/users.json")
+//Despliega los cohorts en el menu desplegable -
+fetch (cohortsJson)
+        .then(function(cohorts){
+        return cohorts.json();
+        })
+        .then(function(list){
+        const promo = list;
+        console.log(promo.length);
+        for (let i = 0; i<promo.length; i++ ){
+            const options = document.createElement("option");
+            const containOp = document.createTextNode(promo[i].id);
+            options.appendChild(containOp);
+            console.log(promo[i].id);
+            menu.appendChild(options);
+
+        }
+        });
+
+//Muestra a las alumnas
+
+
+botonDeLista.onclick = function () {
+    if (menu.value === "lim-2018-03-pre-core-pw"){
+    
+    fetch(usersJson)
     .then(function(response){
         return response.json();
     })
@@ -18,6 +46,8 @@ fetch("../data/cohorts/lim-2018-03-pre-core-pw/users.json")
         })
     ;
     }
+    }
+
     /*  lista completa de los readyStatevalores está documentada en XMLHTTPRequest.readyState y es la siguiente:
 0 (sin inicializar) o ( solicitud no inicializada )
 1 (carga) o ( conexión al servidor establecida )
@@ -34,22 +64,6 @@ fetch("../data/cohorts/lim-2018-03-pre-core-pw/users.json")
         });
 */
 
-fetch ("../data/cohorts.json")
-        .then(function(cohorts){
-        return cohorts.json();
-        })
-        .then(function(list){
-        const promo = list;
-        console.log(promo.length);
-        for (let i = 0; i<promo.length; i++ ){
-            const options = document.createElement("option");
-            const containOp = document.createTextNode(promo[i].id);
-            options.appendChild(containOp);
-            console.log(promo[i].id);
-            menu.appendChild(options);
-
-        }
-        });
 
 /*form.addEventListener("submit", function(e){
     e.preventDefault();
