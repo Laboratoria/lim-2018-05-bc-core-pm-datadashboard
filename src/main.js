86e5a4e953eca25d181c04a264689aec3ac9c11c
YennyCastillo <aricrisf@hotmail.com>
2018-06-26 18:26:18 -0500

    nuevo main

--- src/main.js
+++ src/main.js
@@ -1,23 +1,21 @@
 
-const selectElement = document.getElementById("Cohorts");
-extraerJSON("../data/cohorts.json", (err, json) => {
+const selectElement = document.getElementById('cohorts');
+extraerJSON('../data/cohorts.json', (err, json) => {
   if (err) {
     // algo salio mal...
     return console.log(); (err);
   }
   const sedes = json;
   console.log(sedes);
-  
    for(let i =0; i< sedes.length; i++){
-     
-    
-    const liElemnts=document.createElement("li");
+     if(sedes[i].id.substr(0,3)=== 'lim'){
+    const liElements = document.createElement('li');
     liElements.textContent= sedes[i].id;
-    selectElement.appendChild(liElemnts);
-   } 
+    selectElement.appendChild(liElements);
+   } }
 
-  const selectElementU = document.getElementById("users");
-  extraerJSON("../data/cohorts/lim-2018-03-pre-core-pw/users.json", (err, json) => {
+  const selectElementU = document.getElementById('users');
+  extraerJSON('../data/cohorts/lim-2018-03-pre-core-pw/users.json', (err, json) => {
     if (err) {
       // algo salio mal...
       return console.log(); (err);
@@ -25,15 +23,15 @@ extraerJSON("../data/cohorts.json", (err, json) => {
     const users = json;
     console.log(users);
        for(let i =0; i< users.length; i++){
-        const optionsElemntsU=document.createElement("option");
+        const optionsElemntsU=document.createElement('option');
         const contenidoOptionsU= document.createTextNode(users[i].name);
         optionsElemntsU.appendChild(contenidoOptionsU);
         selectElementU.appendChild(optionsElemntsU);
           
           } 
 
-    const selectElementP = document.getElementById("progress");
-    extraerJSON("../data/cohorts/lim-2018-03-pre-core-pw/progress.json", (err, jsonP) => {
+    const selectElementP = document.getElementById('progress');
+    extraerJSON('../data/cohorts/lim-2018-03-pre-core-pw/progress.json', (err, jsonP) => {
       if (err) {
         // algo salio mal...
         // return console.error(err);
@@ -46,12 +44,12 @@ extraerJSON("../data/cohorts.json", (err, json) => {
 
       for (let i = 0; i < 5; i++) {
 
-/*         const optionsElemntsP=document.createElement("li");
+/*         const optionsElemntsP=document.createElement('li');
         const contenidoOptionsP= document.createTextNode(progress[[i].id].intro.units.parts[6].exercises.completed);
         optionsElemntsP.appendChild(contenidoOptionsP);
         selectElementP.appendChild(optionsElemntsP);
         console.log(progress[i].id.intro.units.parts.exercises.completed); */
-        console.log(progress[users[i].id].intro.units["02-variables-and-data-types"].parts["04-guided-exercises"].completed);
+        console.log(progress[users[i].id].intro.units['02-variables-and-data-types'].parts['04-guided-exercises'].completed);
         
 
 
@@ -69,17 +67,17 @@ extraerJSON("../data/cohorts.json", (err, json) => {
 
 
 
-/*extractJSON("../data/cohorts.json")
+/*extractJSON('../data/cohorts.json')
 
-extractJSON("../data/cohorts/lim-2018-03-pre-core-pw/users.json")
+extractJSON('../data/cohorts/lim-2018-03-pre-core-pw/users.json')
 
-extractJSON("../data/cohorts/lim-2018-03-pre-core-pw/progress.json")
+extractJSON('../data/cohorts/lim-2018-03-pre-core-pw/progress.json')
 const sedes= json;
 
-const optionsElemnts=document.createElement("option");
+const optionsElemnts=document.createElement('option');
 const contenidoOptions= document.createTextNode(sedes[i].id);
 optionsElemnts.appendChild(contenidoOptions);
 selectElement.appendChild(optionsElemnts);
 console.log(sedes[i].id);
-document.getElementById("sedes").appendChild(optionsElemnts);
+document.getElementById('sedes').appendChild(optionsElemnts);
 */
