f230baccbc96ddca6dcf67b9906d582e60088229
jslyne <jslyn.vip@gmail.com>
2018-07-03 14:26:58 -0500

    Arreglo de directorios

--- src/assets/js/login.js
+++ src/assets/js/login.js
@@ -3,7 +3,7 @@ function validar() {
   let password = document.getElementById('password').value;
 
   if (usuario === 'alejandra' && password === 'admin') {
-    return window.location='index.html'
+    return window.location='assets/index.html'
   }
   else {
     alert ('Verifique sus datos');
